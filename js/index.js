bf6536e8b889db1b6a780fc1f33b21ca0008e3d3
Del1rius <timothy.aidan.barry@gmail.com>
2023-05-16 14:43:04 +0200

    js group exercise

---
+++ js/index.js
@@ -0,0 +1,41 @@
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 4;
+let num5 = 5;
+let num6 = 6;
+let num7 = 7;
+let num8 = 8;
+let num9 = 9;
+let num10 = 10;
+
+document.write(num1 , num2, num3, num4, "<br>");
+document.write(num5, num6, num7, "<br>");
+document.write(num8, num9, num10, "<br>");
+document.write("Hello Everyone thank you for coming to my ted talk");
+
+let person = {
+    p1: 1,
+    p2: 2,
+    p3: 3,
+    p4: 4,
+    p5: 5,
+    p6: 6,
+    p7: 7,
+    p8: 8,
+    p9: 9,
+    p10: 10
+};
+document.write(`<br> ${person.p1, person.p2, person.p3, person.p4} <br> ${person.p5, person.p6, person.p7} <br> ${person.p8, person.p9, person.p10}`)
+
+
+// let text = "";
+
+// for (let i = 0; i < 3, i++;){
+//     text += "The number is " + i + "<br>";
+
+// }
+// document.getElementById("demo").innerHTML = text;   
+
+
+
